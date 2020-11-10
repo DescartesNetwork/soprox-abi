b190d8413b7a74620ab41be2e02a9df1dfd265e6
tphanson <tphanson@kambria.io>
2020-11-10 16:17:32 +0700

    add pub

--- src/types/index.js
+++ src/types/index.js
@@ -4,6 +4,7 @@ const { u8, u16, u32, u64 } = require('./usize');
 const { i8, i16, i32, i64 } = require('./isize');
 const { array } = require('./array');
 const { tuple } = require('./tuple');
+const { pub } = require('./pub');
 
 module.exports = {
   bool,
@@ -12,4 +13,5 @@ module.exports = {
   u8, u16, u32, u64,
   array,
   tuple,
+  pub,
 };
