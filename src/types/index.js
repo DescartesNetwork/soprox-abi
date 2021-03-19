fa6a4ff794071576093aa872285c48ffc32ddd1b
tphanson <tphanson@kambria.io>
2021-03-19 22:51:36 +0700

    support u128

--- src/types/index.js
+++ src/types/index.js
@@ -1,6 +1,6 @@
 const { bool } = require('./bool');
 const { char } = require('./char');
-const { u8, u16, u32, u64 } = require('./usize');
+const { u8, u16, u32, u64, u128 } = require('./usize');
 const { i8, i16, i32, i64 } = require('./isize');
 const { array } = require('./array');
 const { tuple } = require('./tuple');
@@ -9,8 +9,8 @@ const { pub } = require('./pub');
 module.exports = {
   bool,
   char,
+  u8, u16, u32, u64, u128,
   i8, i16, i32, i64,
-  u8, u16, u32, u64,
   array,
   tuple,
   pub,
