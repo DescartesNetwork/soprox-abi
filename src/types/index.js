b26dc1968255ec6c319914a4f107182fc3b7cbc4
tuphan <tuphan@descartes.network>
2021-06-07 17:38:07 +0700

    i128

--- src/types/index.js
+++ src/types/index.js
@@ -1,7 +1,7 @@
 const { bool } = require('./bool');
 const { char } = require('./char');
 const { u8, u16, u32, u64, u128 } = require('./usize');
-const { i8, i16, i32, i64 } = require('./isize');
+const { i8, i16, i32, i64, i128 } = require('./isize');
 const { array } = require('./array');
 const { tuple } = require('./tuple');
 const { pub } = require('./pub');
@@ -10,7 +10,7 @@ module.exports = {
   bool,
   char,
   u8, u16, u32, u64, u128,
-  i8, i16, i32, i64,
+  i8, i16, i32, i64, i128,
   array,
   tuple,
   pub,
--- src/types/isize.js
+++ src/types/isize.js
@@ -6,11 +6,14 @@ const BN = require('bn.js');
 const ONE = new BN(1);
 const NEONE = new BN(-1);
 const F = new BN(Buffer.from('ffffffffffffffff', 'hex'), 16, 'le');
-const MAX = new BN(Buffer.from('ffffffffffffff7f', 'hex'), 16, 'le');
-const MIN = MAX.mul(NEONE).add(NEONE);
+const FMAX = new BN(Buffer.from('ffffffffffffff7f', 'hex'), 16, 'le');
+const FMIN = FMAX.mul(NEONE).add(NEONE);
+const FF = new BN(Buffer.from('ffffffffffffffffffffffffffffffff', 'hex'), 16, 'le');
+const FFMAX = new BN(Buffer.from('ffffffffffffffffffffffffffffff7f', 'hex'), 16, 'le');
+const FFMIN = FFMAX.mul(NEONE).add(NEONE);
 Buffer.prototype.writeBigInt64LE = Buffer.prototype.writeBigInt64LE || function (jsBigInt, offset = 0) {
   const bigInt = new BN(jsBigInt.toString());
-  if (bigInt.gt(MAX) || bigInt.lt(MIN)) throw new Error('BigInt is too big');
+  if (bigInt.gt(FMAX) || bigInt.lt(FMIN)) throw new Error('BigInt is too big');
   const reverseBigInt = F.add(bigInt).add(ONE);
   const arrayBuf = reverseBigInt.toArray('le', this.length);
   for (let i = 0; i < this.length; i++)
@@ -20,7 +23,23 @@ Buffer.prototype.writeBigInt64LE = Buffer.prototype.writeBigInt64LE || function 
 Buffer.prototype.readBigInt64LE = Buffer.prototype.readBigInt64LE || function (offset = 0) {
   let bigInt = new BN(this, 16, 'le');
   if (bigInt.gt(F)) throw new Error('BigInt is too big');
-  if (bigInt.gt(MAX)) bigInt = F.sub(bigInt).add(ONE);
+  if (bigInt.gt(FMAX)) bigInt = F.sub(bigInt).add(ONE);
+  // Using global.BigInt instead of BigInt due to browser understanding
+  return global.BigInt(bigInt.toString());
+}
+Buffer.prototype.writeBigInt128LE = Buffer.prototype.writeBigInt128LE || function (jsBigInt, offset = 0) {
+  const bigInt = new BN(jsBigInt.toString());
+  if (bigInt.gt(FFMAX) || bigInt.lt(FFMIN)) throw new Error('BigInt is too big');
+  const reverseBigInt = FF.add(bigInt).add(ONE);
+  const arrayBuf = reverseBigInt.toArray('le', this.length);
+  for (let i = 0; i < this.length; i++)
+    this[i] = arrayBuf[i];
+  return offset;
+}
+Buffer.prototype.readBigInt128LE = Buffer.prototype.readBigInt128LE || function (offset = 0) {
+  let bigInt = new BN(this, 16, 'le');
+  if (bigInt.gt(FF)) throw new Error('BigInt is too big');
+  if (bigInt.gt(FFMAX)) bigInt = FF.sub(bigInt).add(ONE);
   // Using global.BigInt instead of BigInt due to browser understanding
   return global.BigInt(bigInt.toString());
 }
@@ -37,6 +56,8 @@ const type2Write = (type) => {
       return 'writeInt32LE';
     case 'i64':
       return 'writeBigInt64LE';
+    case 'i128':
+      return 'writeBigInt128LE';
     default:
       throw new Error('Invalid type');
   }
@@ -52,6 +73,8 @@ const type2Read = (type) => {
       return 'readInt32LE';
     case 'i64':
       return 'readBigInt64LE';
+    case 'i128':
+      return 'readBigInt128LE';
     default:
       throw new Error('Invalid type');
   }
@@ -104,4 +127,10 @@ class i64 extends isize {
   }
 }
 
-module.exports = { i8, i16, i32, i64 }
+class i128 extends isize {
+  constructor(value = 0) {
+    super(value, 'i128', 16);
+  }
+}
+
+module.exports = { i8, i16, i32, i64, i128 }
